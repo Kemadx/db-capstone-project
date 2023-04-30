a1fe25ed2c4b039117eabbb46013f56d9dbaab01
Kenneth Q Madsen <kemad06@gmail.com>
2023-04-30 20:35:43 +0200

    Testdata to insert

---
+++ Instert_Testdata.sql
@@ -0,0 +1,13 @@
+INSERT INTO customers (CustomerID, FullName) VALUES
+(1, "Bjarne Olsen"),
+(2, "Tina Olsen"),
+(3, "James Jørgensen"),
+(4, "Mads Morgenstjerne"),
+(5, "Pernille Pilgaard");
+
+INSERT INTO staff (StaffID, Name, Role, AnnualSalary) VALUES
+(1, "Peter Pedal", "Manager", 550000),
+(2, "Saerun Bethesda", "Waitress", 360000),
+(3, "Torben Bager", "Waiter", 360000),
+(4, "Steffen Didriksen", "Kok", 390000),
+(5, "Frank Johansen", "Souschef", 440000);
