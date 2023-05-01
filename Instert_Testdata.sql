12c29a4e31e4e30f30ad985f06d41660ca0c8a8f
Kenneth Q Madsen <kemad06@gmail.com>
2023-05-01 19:36:03 +0200

    Added booking entries for test

--- Instert_Testdata.sql
+++ Instert_Testdata.sql
@@ -11,3 +11,9 @@ INSERT INTO staff (StaffID, Name, Role, AnnualSalary) VALUES
 (3, "Torben Bager", "Waiter", 360000),
 (4, "Steffen Didriksen", "Kok", 390000),
 (5, "Frank Johansen", "Souschef", 440000);
+
+INSERT INTO bookings (BookingID, TableNo, BookingSlot, StaffID, CustomerID) VALUES
+(1,5,"2022-10-10 18:00",1,1),
+(2,3,"2022-11-12 18:00",3,3),
+(3,2,"2022-10-11 18:00",2,2),
+(4,2,"2022-10-13 18:00",2,2);
